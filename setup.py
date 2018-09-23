5de0415e351aec8e922f17a119f0fb376cc10c14
Zeyad Obaia <zeyad.obaia@gmail.com>
2018-09-23 23:21:13 +0200

    Windows Support

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 import os
 import shutil
 import errno
-
+import platform
 
 def copy(src, dest):
     try:
         shutil.copytree(src, dest, ignore=shutil.ignore_patterns('setup.py', 'db.sqlite3', '.*',
-         '__pycache__', 'pyqt5-admin', 'build', 'dist', '__main__.spec', 'examples'))
+         '__pycache__', 'pyqt-admin*', 'build', 'dist', '__main__.spec', 'examples', 'pyqt-admin.exe'))
     except OSError as e:
         # If the error was caused because the source wasn't a directory
         if e.errno == errno.ENOTDIR:
@@ -18,15 +18,31 @@ def copy(src, dest):
 
 def main():
     home = os.path.expanduser("~")
-    cfg_dir = os.path.join(home, '.pyqt5-django-orm')
+    cfg_dir = os.path.join(home, '.django-pyqt')
     if os.path.exists(cfg_dir):
         shutil.rmtree(cfg_dir)
     src_dir = os.path.abspath('.')
 
     copy(src_dir, cfg_dir)
-    copy('pyqt5-admin', '/usr/bin')
-    os.system('chmod +x /usr/bin/pyqt5-admin')
-
+    if platform.system() == 'Linux':
+        copy('pyqt-admin', '/usr/bin')
+        os.system('chmod +x /usr/bin/pyqt-admin')
+    elif platform.system() == 'Windows':
+        path_to_try = [os.path.join(os.environ['WINDIR'], 'system'),
+                    os.path.join(os.environ['WINDIR'], 'System'),
+                    os.path.join(os.environ['WINDIR'], 'system32'),
+                    os.path.join(os.environ['WINDIR'], 'System32')
+                    ]
+        path = os.environ['PATH'].split(';')
+        used_path = ""
+        for i in path_to_try:
+            if i in path:
+                used_path = i
+                break
+        if len(used_path) == 0:
+            print("Please fix PATH environment variable")
+            return
+        copy('pyqt-admin.exe', used_path)
 
 if __name__ == '__main__':
     main()
